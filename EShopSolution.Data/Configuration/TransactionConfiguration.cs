367d16bbd43dd5a5698c3ec0bae75868583e0050
hua hai <huasyhai@gmail.com>
2020-10-10 14:54:58 +0800

    add entity and configuration and migration database

---
+++ EShopSolution.Data/Configuration/TransactionConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using EShopSolution.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace EShopSolution.Data.Configuration
+{
+    public class TransactionConfiguration : IEntityTypeConfiguration<Entities.Transaction>
+    {
+
+        public void Configure(EntityTypeBuilder<Entities.Transaction> builder)
+        {
+                builder.ToTable("Transactions");
+
+                builder.HasKey(x => x.Id);
+
+                builder.Property(x => x.Id).UseIdentityColumn();
+        }
+    }
+}
