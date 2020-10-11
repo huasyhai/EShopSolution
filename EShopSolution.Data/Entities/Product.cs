a098ea0ce0e6db35372f7cfaecd40c185bc21520
hua hai <huasyhai@gmail.com>
2020-10-11 11:24:45 +0800

    seeding data

--- EShopSolution.Data/Entities/Product.cs
+++ EShopSolution.Data/Entities/Product.cs
@@ -18,8 +18,6 @@ namespace EShopSolution.Data.Entities
 
         public DateTime DateCreated { get; set; }
 
-        public string SeoAlias { get; set; }
-
         public List<ProductInCategory> ProductInCategories { get; set; }
 
         public List<OrderDetail> OrderDetails { get; set; }
