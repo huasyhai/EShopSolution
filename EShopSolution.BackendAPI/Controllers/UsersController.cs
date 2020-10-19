ac44c4f6b2cdfbefb005093a52307d15b3baa81e
hua hai <huasyhai@gmail.com>
2020-10-19 09:41:59 +0800

    add jwt

---
+++ EShopSolution.BackendAPI/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EShopSolution.Application.System.Users;
+using EShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShopSolution.BackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("Authenticate")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Authenticate([FromForm] LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var resultToken = await _userService.Authenticate(request);
+
+            if(String.IsNullOrEmpty(resultToken))
+            {
+                return BadRequest("UserName or Password is incorrect");
+            }
+
+            return Ok(new { Token = resultToken });
+        }
+
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _userService.Register(request);
+
+            if (result)
+                return Ok();
+
+            return BadRequest("Register is unsuccessfull");
+        }
+    }
+}   
